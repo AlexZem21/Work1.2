3a6eff8b8e2aedc52f524cfbe1bb66a0e35fac2d
AlexZem21 <azemskov002@gmail.com>
2026-03-10 09:04:14 +0800

    initial version

--- Work1.2/Program.cs
+++ Work1.2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, -4, 12, 56, -4 };
+        int count = 0;
+
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            if (numbers[i] * numbers[i + 1] < 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Исходная последовательность: {" + string.Join(", ", numbers) + "}");
+        Console.WriteLine("Количество смен знака: " + count);
+    }
+}
